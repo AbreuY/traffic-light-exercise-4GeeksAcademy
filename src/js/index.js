a76c8cabb7c605ff733b5c1a87a6ee8a6205a2c2
AbreuY <yolfran12@gmail.com>
2022-04-23 00:11:21 +0000

    Removed import custom.css

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,6 @@ import "bootstrap";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
-import "../styles/custom.css"; //import your own components
 import Home from "./component/home.jsx";
 
 //render your react application
