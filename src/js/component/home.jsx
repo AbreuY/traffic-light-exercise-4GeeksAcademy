cbb87d3599ba1b0f53bfa2c06c4b8f984807758c
AbreuY <yolfran12@gmail.com>
2022-04-23 00:07:36 +0000

    Removed unused code

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,36 +7,7 @@ import { TrafficLight } from "./TrafficLight1.jsx";
 //create your first component
 
 const Home = () => {
-	const [selectRed, setSelectedRed] = useState(false);
-	const [selectYellow, setSelectedYellow] = useState(false);
-	const [selectGreen, setSelectedGreen] = useState(false);
-
-	return (
-		<TrafficLight />
-		/* <div className="verticalRectangle">
-			<div
-				className={`red ${selectRed ? "light" : ""}`}
-				onClick={e => {
-					setSelectedRed(!selectRed);
-					setSelectedYellow(false);
-					setSelectedGreen(false);
-				}}></div>
-			<div
-				className={`yellow ${selectYellow ? "light" : ""}`}
-				onClick={e => {
-					setSelectedYellow(!selectYellow);
-					setSelectedRed(false);
-					setSelectedGreen(false);
-				}}></div>
-			<div
-				className={`green ${selectGreen ? "light" : ""}`}
-				onClick={e => {
-					setSelectedGreen(!selectGreen);
-					setSelectedRed(false);
-					setSelectedYellow(false);
-				}}></div>
-		</div> */
-	);
+	return <TrafficLight />;
 };
 
 export default Home;
